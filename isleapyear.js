39901ae7265c401b6c2779ad6b985015fcd3bf91
Pritom Das <pritom6221@gmail.com>
2023-08-09 00:12:07 +0600

    some changes added

--- isleapyear.js
+++ isleapyear.js
@@ -1,12 +1,17 @@
 function isLeapyear(year) {
     const remainder = year % 4;
     if(remainder == 0){
-        console.log('Yes, It is a Leap Year', year)
+        // console.log('Yes, It is a Leap Year', year)
+        return true;
     }
     else {
-        console.log('Ops, It is not a Leap Year', year)
+        // console.log('Ops, It is not a Leap Year', year)
+        return false;
     }
 }
-isLeapyear(2022);
-isLeapyear(2024);
-isLeapyear(2020);
+
+const isMyYearIsLeapYear = isLeapyear(2022);
+console.log('my year:', isMyYearIsLeapYear)
+
+const isHerYearIsLeapyear = isLeapyear(2024);
+console.log('her year:', isHerYearIsLeapyear)
