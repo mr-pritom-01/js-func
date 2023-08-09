410a49d75ea048f5ef46ce7d5c84ddbffbe58b3a
Pritom Das <pritom6221@gmail.com>
2023-08-09 12:55:29 +0600

    factorial learned

--- findOddSum.js
+++ findOddSum.js
@@ -1,3 +1,5 @@
+// (20 - 5/6)
+
 function getSumOfArray(numbers){
     //  console.log(numbers);
     let sum = 0;
@@ -11,17 +13,22 @@ function getSumOfArray(numbers){
 }
 
 function getOddNumbersOfArray(numbers){
+    const oddNumbers = [];
      for(let i = 0; i < numbers.length; i++){
         const index = i;
         const element = numbers[index];
         if(element % 2 !== 0){
             console.log(index, element);
+            oddNumbers.push(element);
         }
      }
+     return oddNumbers;
 }
 
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
 
-getOddNumbersOfArray(myNumbers);
+const oddNumbers = getOddNumbersOfArray(myNumbers);
+console.log(oddNumbers);
+const oddNumbersSum = getSumOfArray(oddNumbers);
 
-getSumOfArray(myNumbers);
+console.log('odd number sum', oddNumbersSum)
