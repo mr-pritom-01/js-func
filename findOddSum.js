42db4cb6efe2f9109b4b922035f46442498903bf
Pritom Das <pritom6221@gmail.com>
2023-08-09 11:40:25 +0600

    find odd sum js added

---
+++ findOddSum.js
@@ -0,0 +1,11 @@
+function getSumOfArray(numbers){
+    //  console.log(numbers);
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index]
+        console.log(index, element);
+    }
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+getSumOfArray(myNumbers);
